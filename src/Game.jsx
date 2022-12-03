9ec59f744e0abc5e3ae40de2d6fb0e47e8c0c5f4
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-12-03 15:33:15 -0300

    Change game component structure, creating new components for each div

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
 import "./game.css";
 
+
+
+function Square (props) {
+  let image = "../public/images/" + props.squareID + ".jpg"
+  return <div className="square" id={props.squareID} onClick={props.clickHandler} style={{
+    backgroundImage: "url(" + image + ")",
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }}></div>
+}
+
 function Game(props) {
   const [clicked, setClicked] = useState([]);
+
+  const squareList = [];
+  for(let i = 0;i < 10;i++) {
+    squareList.push(<Square squareID={i} clickHandler={handleClick} key={i}/>);
+  }
+
 
   function setStreak(cmd) {
     if (cmd === true) {
@@ -26,17 +44,8 @@ function Game(props) {
 
   return (
     <>
-      <div id="quadrados">
-        <div className="quadrado" id="1" onClick={handleClick}>1</div>
-        <div className="quadrado" id="2" onClick={handleClick}>2</div>
-        <div className="quadrado" id="3" onClick={handleClick}>3</div>
-        <div className="quadrado" id="4" onClick={handleClick}>4</div>
-        <div className="quadrado" id="5" onClick={handleClick}>5</div>
-        <div className="quadrado" id="6" onClick={handleClick}>6</div>
-        <div className="quadrado" id="7" onClick={handleClick}>7</div>
-        <div className="quadrado" id="8" onClick={handleClick}>8</div>
-        <div className="quadrado" id="9" onClick={handleClick}>9</div>
-        <div className="quadrado" id="10" onClick={handleClick}>10</div>
+      <div id="squares">
+          {squareList}
       </div>
     </>
   );
